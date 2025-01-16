ca419f14cf3432575f233a1122f60d5f09d0e356
harshsharmaaiinfox <harsh.sharma@aiinfox.com>
2025-01-16 17:01:01 +0530

    env URL Change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   baseURL: 'http://localhost:4200', 
-  URL: 'https://api.bogomenus.com/public/api', // Change only the domain part, keeping "/api" intact
-  storageURL: 'https://api.bogomenus.com/public' // Change only the laravel primary domain
+  URL: 'https://api.fashioncarft.com/public/api', // Change only the domain part, keeping "/api" intact
+  storageURL: 'https://api.fashioncarft.com/public/' // Change only the laravel primary domain
 };
 
 /*
