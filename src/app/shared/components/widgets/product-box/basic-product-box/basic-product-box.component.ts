32a5f3f1dd6a6f66c64e8d152e1cea47d3707097
harshsharmaaiinfox <harsh.sharma@aiinfox.com>
2025-02-07 22:25:30 +0530

    rendom star rating

--- src/app/shared/components/widgets/product-box/basic-product-box/basic-product-box.component.ts
+++ src/app/shared/components/widgets/product-box/basic-product-box/basic-product-box.component.ts
@@ -38,7 +38,7 @@ export class BasicProductBoxComponent {
 	}
 
   ngOnInit() {
-    console.log(this.product);
+    this.product.rating_count = Math.floor(Math.random() * 3) + 3;
     this.cartItem$.subscribe(items => {
       this.cartItem = items.find(item => item.product.id == this.product.id)!;
     });
