feb40ea463f143e808460bcf537255d1a973df6e
harshsharmaaiinfox <harsh.sharma@aiinfox.com>
2025-02-27 23:23:25 +0530

    contcat us changes

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,31 +1,13 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-us',
+  standalone: true,
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.scss']
+  styleUrls: ['./contact-us.component.scss'],
 })
 export class ContactUsComponent {
-  contactForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.contactForm = this.fb.group({
-      firstName: ['', [Validators.required, Validators.pattern('^[A-Za-z]+$')]],
-      lastName: ['', [Validators.required, Validators.pattern('^[A-Za-z]+$')]],
-      email: ['', [Validators.required, Validators.email]],
-      phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
-      query: ['', [Validators.required, Validators.minLength(10)]]
-    });
-  }
-
-  onSubmit() {
-    if (this.contactForm.valid) {
-      console.log('Form submitted:', this.contactForm.value);
-      alert('Form submitted successfully!');
-      this.contactForm.reset();
-    } else {
-      alert('Please fill all fields correctly before submitting.');
-    }
-  }
 }
