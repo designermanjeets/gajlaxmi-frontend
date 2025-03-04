e92d4bb45e5d50aae06c1e2a33bfa35f58cbbb11
harshsharmaaiinfox <harsh.sharma@aiinfox.com>
2025-03-04 14:38:14 +0530

    change menu and category names

--- src/app/shared/state/category.state.ts
+++ src/app/shared/state/category.state.ts
@@ -4,6 +4,7 @@ import { tap } from "rxjs";
 import { GetCategories, GetCategoryBySlug, GetSearchByCategory } from "../action/category.action";
 import { Category } from "../../shared/interface/category.interface";
 import { CategoryService } from "../services/category.service";
+import { categoryOptionsGajLaxmi } from "../data/categoryOptions";
 
 export class CategoryStateModel {
   category = {
@@ -52,7 +53,7 @@ export class CategoryState {
         next: result => { 
           ctx.patchState({
             category: {
-              data: result.data,
+              data: categoryOptionsGajLaxmi,
               total: result?.total ? result?.total : result.data.length
             }
           });
